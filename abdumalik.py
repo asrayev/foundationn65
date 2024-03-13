22b841a9546ccf167d59b61253ab42a0a36fdeac
soliyev23 <asoliyev53@gmail.com>
2024-03-13 09:47:23 +0500

    nma gap

--- abdumalik.py
+++ abdumalik.py
@@ -1,17 +1,19 @@
-x = int(input("--->"))
-print(("Tub") if len([i for i in range(1,x+1) if x%i==0])==2 else ("Tub emas"))
+# x = int(input("--->"))
+# print(("Tub") if len([i for i in range(1,x+1) if x%i==0])==2 else ("Tub emas"))
 
 # 1 - MISOL
-lst = input("Enter the names:").split()
-for i in range(len(lst)):
-    if len(lst)-1 == i:
-        print(f"and {lst[i]} like this",end="")
-    elif len(lst)-2 == i:
-        print(f"{lst[i]} ",end="")
-    else:
-        print(f"{lst[i]},",end="")
+# lst = input("Enter the names:").split()
+# for i in range(len(lst)):
+#     if len(lst)-1 == i:
+#         print(f"and {lst[i]} like this",end="")
+#     elif len(lst)-2 == i:
+#         print(f"{lst[i]} ",end="")
+#     else:
+#         print(f"{lst[i]},",end="")
 
 # 1 MIOSL ternary if
 # lst = input("Enter the names:").split()
 # for i in range(len(lst)):
 #     print(f"and {lst[i]} like this",end="") if len(lst)-1 == i else print(f"{lst[i]} ",end="") if len(lst)-2 == i else print(f"{lst[i]},",end="")
+
+
